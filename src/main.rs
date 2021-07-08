8003ae46a5359d30fe3270a8ce522fe82e0be7b3
hannydevelop <ukpaiugochiibem@gmail.com>
2021-07-08 11:42:15 +0100

    Adding documentation

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,10 @@ impl ConvertNumber<i64> for Integer32 {
     }
 }
 
-//! I was trying out `impl` and `traits` and thought it was cool to build something from it.
-//! I'm using the Rust `ceil` method to round up Rust numbers. Since I'm collecting `args` with Strings `collect` method, args is String.
-//! I use the `parse` method to parse String to integer, then finally round it up to i32.
-//! For now, this can only convert `i64` type to `i32`. The goal in future is to convert an `f32` or `f64` type to `i32` type.
+/// I was trying out `impl` and `traits` and thought it was cool to build something from it.
+/// I'm using the Rust `ceil` method to round up Rust numbers. Since I'm collecting `args` with Strings `collect` method, args is String.
+/// I use the `parse` method to parse String to integer, then finally round it up to i32.
+/// For now, this can only convert `i64` type to `i32`. The goal in future is to convert an `f32` or `f64` type to `i32` type.
 
 fn convert_number<T: ConvertNumber<i64>>(t: &T) {
     println!("Your float number is now an integer: {}", t.convert_number())
