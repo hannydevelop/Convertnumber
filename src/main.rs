4da22cad1a945fbecd80635a8f26b3ddd882fb90
hannydevelop <ukpaiugochiibem@gmail.com>
2021-07-08 11:41:45 +0100

    Adding documentation

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
 //! This is a simple crate to illustrate number conversion from float to integer.
 
 
-//! I was trying out `impl` and `traits` and thought it was cool to build something from it.
-//! I'm using the Rust `ceil` method to round up Rust numbers. Since I'm collecting `args` with Strings `collect` method, args is String.
-//! I use the `parse` method to parse String to integer, then finally round it up to i32.
-//! For now, this can only convert `i64` type to `i32`. The goal in future is to convert an `f32` or `f64` type to `i32` type.
-
-
 //! Install this crate with `cargo install convertnumber`.
 //! To convert your number, pass it from the command line interface. For instance, Run `cargo run 20.10` to get the output `21`.
 
@@ -25,6 +19,11 @@ impl ConvertNumber<i64> for Integer32 {
         self.number.clone()
     }
 }
+
+//! I was trying out `impl` and `traits` and thought it was cool to build something from it.
+//! I'm using the Rust `ceil` method to round up Rust numbers. Since I'm collecting `args` with Strings `collect` method, args is String.
+//! I use the `parse` method to parse String to integer, then finally round it up to i32.
+//! For now, this can only convert `i64` type to `i32`. The goal in future is to convert an `f32` or `f64` type to `i32` type.
 
 fn convert_number<T: ConvertNumber<i64>>(t: &T) {
     println!("Your float number is now an integer: {}", t.convert_number())
